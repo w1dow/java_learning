04aaf8b2a39cd95c6f0653341d793f2d43b276fd
w1dow <ts1857645@gmail.com>
2024-05-18 14:41:34 +0530

    ads

--- java_1.java
+++ java_1.java
@@ -1,3 +1,12 @@
-class java_1{
-    main
+/**
+ * java_1
+ */
+public class java_1 {
+
+    public static void main(String[] args) {
+        System.err.println("Hello world");
+        for (int index = 0; index < 6; index++) {
+            System.err.println(index);
+        }
+    }
 }
